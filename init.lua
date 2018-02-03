653f50ce4af1a0772eaa3ff68ca7d1a0bbef7971
pandaro <prandipadaro@gmail.com>
2018-02-04 00:03:57 +0100

    update

--- init.lua
+++ init.lua
@@ -1,23 +1,7 @@
 rpg_tool={}
-
+rpg_tool.swap=''
 local timer = 0
-local tool_set = function(meta, player,caps)
-
-	local new_uses = caps.groupcaps.cracky.uses + player:get_attribute('durability')
-	local new_time_3 = caps.groupcaps.cracky.times[3] * 1 - (player:get_attribute('tool')/10)
-	local new_time_2 = caps.groupcaps.cracky.times[2] * 1 - (player:get_attribute('tool')/10)
-	local new_damage = caps.damage_groups.fleshy * 1 + (player:get_attribute('sword')/10)
 	
---print(tostring(caps.groupcaps.cracky.uses))
-	meta:set_tool_capabilities({
- 		max_drop_level=0,
- 		groupcaps={
- 			cracky={times={[2]=0.1, [3]=0.1}, uses=new_uses, maxlevel=1}
- 		},
- 		damage_groups = {fleshy=new_damage},
- 	})
-	
-end	
 
 minetest.register_globalstep(function(dtime)
 	timer = timer + dtime;
@@ -46,25 +30,44 @@ minetest.register_globalstep(function(dtime)
 		timer = 0
 	end
 end)
+minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
+		print('punchnode')
+		local name = puncher:get_player_name()
+		local itemstack= puncher:get_wielded_item()
+		local meta = itemstack:get_meta()
+		meta:set_int('palette_index',1)
+		local owner = meta:set_string('owner',name)
+		local caps = itemstack:get_definition().tool_capabilities
+		tool_set(meta,puncher,caps)
+		return itemstack
+	end)
 
-
-
+local tool_set = function(meta, player,caps)
+	local new_uses = caps.groupcaps.cracky.uses + player:get_attribute('durability')
+	local new_time_3 = caps.groupcaps.cracky.times[3] * 1 - (player:get_attribute('tool')/10)
+	local new_time_2 = caps.groupcaps.cracky.times[2] * 1 - (player:get_attribute('tool')/10)
+	local new_damage = caps.damage_groups.fleshy * 1 + (player:get_attribute('sword')/10)
+	meta:set_tool_capabilities({
+ 		max_drop_level=0,
+ 		groupcaps={
+ 			cracky={times={[2]=0.1, [3]=0.1}, uses=new_uses, maxlevel=1}
+ 		},
+ 		damage_groups = {fleshy=new_damage},
+ 	})
+	
+end
+	
 minetest.register_tool("rpg_tool:1", {
 	description = "rpg_tool 1",
 	inventory_image = "default_tool_steelaxe.png",
-	--inventory_overlay = 'rpg_adapt.png',
-	--color = "0xFFFFFF5A",
 	palette='m.png',
 	base_tool = 'rpg_tool:2',
-	on_use = function(itemstack, user, pointed_thing)
-		
-		return minetest.node_dig(pointed_thing.above, minetest.get_node(pointed_thing.above), user)
-	end,
-
 	on_place = function(itemstack, placer, pointed_thing)
 		print('on_placestart')
+		print(tostring(itemstack:to_table()))
 		local name = placer:get_player_name()
 		local meta = itemstack:get_meta()
+		rpg_tool.swap = meta
 		meta:set_int('palette_index',1)
 		local owner = meta:set_string('owner',name)
 		local caps = itemstack:get_definition().tool_capabilities
