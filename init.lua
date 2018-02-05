43546c118ab358ae495ec4271e43908398e76591
pandaro <prandipadaro@gmail.com>
2018-02-05 22:27:31 +0100

    rework a lot

--- init.lua
+++ init.lua
@@ -1,8 +1,16 @@
 rpg_tool={}
-rpg_tool.swap=''
 local timer = 0
 	
-local tool_set = function(meta, player,caps)
+rpg_tool.tool_set = function(self,stack,player,external)
+print(dump(stack:to_table()))
+	print(dump(player:get_player_name()))
+	--local stack = player:get_wielded_item()
+	local rpgTool = stack:get_definition()._rpg
+	if not rpgTool then return end
+ 	local meta = stack:get_meta()
+	local caps = stack:get_definition().tool_capabilities
+	meta:set_int('palette_index',1)
+	meta:set_string('owner',player:get_player_name())
 	local new_uses = caps.groupcaps.cracky.uses + player:get_attribute('durability')
 	local new_time_3 = caps.groupcaps.cracky.times[3] * 1 - (player:get_attribute('tool')/10)
 	local new_time_2 = caps.groupcaps.cracky.times[2] * 1 - (player:get_attribute('tool')/10)
@@ -14,74 +22,68 @@ local tool_set = function(meta, player,caps)
  		},
  		damage_groups = {fleshy=new_damage},
  	})
+		return stack
 	
 end
+
+
 minetest.register_globalstep(function(dtime)
 	timer = timer + dtime;
-	if timer >= 10 then
+	if timer >= 1 then
 		-- Send "Minetest" to all players every 5 seconds
 		for i, v in pairs(minetest.get_connected_players())do
+			print(' globalstep')
 			local stack= v:get_wielded_item()
-			local stackname = stack:get_name()
-			local rt = string.split(stackname,':')[1]
-			print(tostring(stackname)..' globalstep')
-			print(dump(rt))
-			if rt == 'rpg_tool' then
+			if stack:get_definition()._rpg then
 				local meta = stack:get_meta()
 				local name = meta:get_string('owner')
 				print(tostring('name '..name))
-				
 				if name ~= v:get_player_name() and name ~= '' then
-					local caps = stack:get_definition().tool_capabilities
-
-					v:set_wielded_item({name = stackname,wear = stack:get_wear(),meta=nil})
+					local wear = stack:get_wear()
+					local stackname = stack:get_name()
+					v:set_wielded_item({name = stackname, wear = wear, meta=nil})
 					print(' global STOP')
-					
 				end
 			end
 		end
 		timer = 0
 	end
 end)
-minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
-		print('punchnode')
-		local name = puncher:get_player_name()
-		local itemstack= puncher:get_wielded_item()
-		local meta = itemstack:get_meta()
-		meta:set_int('palette_index',1)
-		local owner = meta:set_string('owner',name)
-		local caps = itemstack:get_definition().tool_capabilities
-		meta:set_tool_capabilities({
- 		max_drop_level=0,
- 		groupcaps={
- 			cracky={times={[2]=0.1, [3]=0.1}, uses=1000, maxlevel=1}
- 		},
- 		damage_groups = {fleshy=2},
-		})
-		puncher:set_wielded_item(itemstack)
-		return itemstack
-	end)
-
-
+-- minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
+-- 		print('punchnode')
+-- 		local stack = puncher:get_wielded_item()
+-- 		local new_stack = rpg_tool:tool_set(stack,puncher)
+-- 		puncher:set_wielded_item(new_stack)
+-- 		
+-- 	end)
+-- 
+-- minetest.register_on_punchplayer(function(player, hitter)
+-- 	print(tostring('punchplayer'))
+-- 	local stack = hitter:get_wielded_item()
+-- 	local new_stack = rpg_tool:tool_set(stack,hitter)
+-- 	hitter:set_wielded_item(new_stack)
+-- 	
+-- 
+-- end)
 	
 minetest.register_tool("rpg_tool:1", {
 	description = "rpg_tool 1",
 	inventory_image = "default_tool_steelaxe.png",
 	palette='m.png',
+	_rpg = true,
 	base_tool = 'rpg_tool:2',
 
 	on_place = function(itemstack, placer, pointed_thing)
 		print('on_placestart')
-		print(tostring(itemstack:to_table()))
-		local name = placer:get_player_name()
-		local meta = itemstack:get_meta()
-		rpg_tool.swap = meta
-		meta:set_int('palette_index',1)
-		local owner = meta:set_string('owner',name)
-		local caps = itemstack:get_definition().tool_capabilities
-		tool_set(meta,placer,caps)
+		rpg_tool:tool_set(itemstack,placer)
 		return itemstack
 	end,
+	on_secondary_use = function(itemstack, user, pointed_thing)
+		print('on_placestart')
+		rpg_tool:tool_set(itemstack,user)
+		return itemstack
+	end,
+
 	tool_capabilities = {
 		max_drop_level=0,
 		groupcaps={
